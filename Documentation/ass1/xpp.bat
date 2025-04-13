919fe4dcda3d77c3511ef08f03ad5b55cf7704ea
TeaNoodle <119759335+Teanoodle@users.noreply.github.com>
2025-04-13 19:25:28 -0400

    Add files via upload

---
+++ Documentation/ass1/xpp.bat
@@ -0,0 +1,8 @@
+set BROWSER=c:\Program Files\Netscape\Communicator\Program\netscape.exe
+set XPPHELP=c:\xppall\help\xpphelp.html
+set DISPLAY=127.0.0.1:0.0
+set HOME=c:\xppall
+c:\xppall\xppaut %1 %2 %3 -grads 0
+pause
+
+
